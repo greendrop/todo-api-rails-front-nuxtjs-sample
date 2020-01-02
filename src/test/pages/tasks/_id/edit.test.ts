8f92ad219eced7b84080da7f963ff3d946248a1a
greendrop <greendrop.devel@gmail.com>
2020-01-02 12:17:19 +0900

    add asyncData test on task edit

--- src/test/pages/tasks/_id/edit.test.ts
+++ src/test/pages/tasks/_id/edit.test.ts
@@ -31,4 +31,102 @@ describe('Edit', () => {
     })
     assert.strictEqual(wrapper.isVisible(), true)
   })
+
+  describe('methods', () => {
+    const tasksStore = {
+      getTaskById: jest.fn(),
+      got: true,
+      task: new Task({ id: 1 })
+    }
+    const toast = {
+      success: jest.fn(),
+      error: jest.fn()
+    }
+    const log = {
+      error: jest.fn()
+    }
+    const route = {
+      params: {
+        id: 1
+      }
+    }
+    const router = {
+      push: jest.fn()
+    }
+    const wrapper = shallowMount(Edit, {
+      i18n,
+      localVue,
+      mocks: {
+        tasksStore,
+        $toast: toast,
+        $log: log,
+        $route: route,
+        $router: router
+      }
+    })
+    const vm = wrapper.vm as any
+
+    describe('asyncData', () => {
+      const context = {
+        route
+      }
+
+      describe('when process.client is true', () => {
+        beforeEach(() => {
+          process.client = true
+        })
+
+        test('returns requestGetTask', async () => {
+          const data = await vm.$options.asyncData(context, {
+            tasksStore
+          })
+          assert.strictEqual(data.requestGetTask, true)
+        })
+      })
+
+      describe('when process.client is false', () => {
+        beforeEach(() => {
+          process.client = false
+        })
+
+        test('returns task', async () => {
+          const data = await vm.$options.asyncData(context, {
+            tasksStore
+          })
+          expect(tasksStore.getTaskById).toHaveBeenCalledWith({
+            id: route.params.id
+          })
+          assert.deepStrictEqual(data.task, tasksStore.task)
+        })
+      })
+    })
+
+    describe('getTask', () => {
+      describe('when got is true', () => {
+        beforeEach(() => {
+          tasksStore.got = true
+        })
+
+        test('not call toast.error', async () => {
+          await vm.getTask()
+          expect(tasksStore.getTaskById).toHaveBeenCalledWith({ id: 1 })
+          expect(toast.error).not.toHaveBeenCalled()
+        })
+      })
+
+      describe('when got is false', () => {
+        beforeEach(() => {
+          tasksStore.got = false
+        })
+
+        test('called toast.error', async () => {
+          await vm.getTask()
+          expect(tasksStore.getTaskById).toHaveBeenCalledWith({
+            id: route.params.id
+          })
+          expect(toast.error).toHaveBeenCalled()
+        })
+      })
+    })
+  })
 })
